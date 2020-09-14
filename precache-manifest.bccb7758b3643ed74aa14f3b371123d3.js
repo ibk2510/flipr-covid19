5cb9cad34f9c8050944e4226216486e8ce95d010
ibk2510 <kalilibrahimjh@gmail.com>
2020-09-14 21:09:00 +0530

    Updates

---
+++ precache-manifest.bccb7758b3643ed74aa14f3b371123d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbd5e2a39f6175fe3c74da851ee091e8",
+    "url": "/flipr-covid19/index.html"
+  },
+  {
+    "revision": "62e0d81fb21de7fa8981",
+    "url": "/flipr-covid19/static/css/main.1b17b5f3.chunk.css"
+  },
+  {
+    "revision": "86f3819c961dd0e4ff63",
+    "url": "/flipr-covid19/static/js/2.4aae55db.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/flipr-covid19/static/js/2.4aae55db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62e0d81fb21de7fa8981",
+    "url": "/flipr-covid19/static/js/main.fbf63bd9.chunk.js"
+  },
+  {
+    "revision": "548f0a81ea715bc1b90b",
+    "url": "/flipr-covid19/static/js/runtime-main.92890752.js"
+  }
+]);
